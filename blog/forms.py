e076cdf632348e56ed2c65bb72e95d10002d7a15
tanvibhakta <tanvibhakta@gmail.com>
2017-11-24 10:58:20 +0530

    accept value from page 1 and move it to page 2

--- blog/forms.py
+++ blog/forms.py
@@ -3,9 +3,11 @@ You probably only need this file if you are havung a form which will write to th
 
 """
 
-
-
 from django import forms
+
+class HomeSearchForm(forms.Form):
+   search_term = forms.CharField(max_length = 100)
+
 # from .models import Owner, Jamroom, OperatingHours, Customer, Booking
 #
 # class HomeSearchForm(forms.ModelForm):
@@ -13,7 +15,3 @@ from django import forms
 #     class Meta:
 #         model = Post
 #         fields = ('title', 'text',)
-
-
-class HomeSearchForm(forms.Form):
-   search_term = forms.CharField(max_length = 100)
--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Owner, Jamroom, OperatingHours, Customer, Booking
 
-# from .models import Owner, Jamroom, OperatingHours, Customer, Booking
 from blog.forms import HomeSearchForm
 
 def home_search(request):
-    search_term = "wydhrvdgwir3t532t7y"
-    MyHomeSearchForm="db5f61g8yh87d4675fb76g"
+    search_result = "search_result wasn't initialised"
+    MyHomeSearchForm="MyHomeSearchForm wasn't initialised"
     if request.method == "POST":
-        print("triggered")
         MyHomeSearchForm = HomeSearchForm(request.POST)
         if MyHomeSearchForm.is_valid():
-            # search_term = MyHomeSearchForm.g
-    # return response(request, 'blog/search_result.html' , { 'search_term': search_term})
-            return render(request, 'blog/search_result.html', {"search_term" : search_term})
+            search_result = request.POST.get("search_term")
+            search_result_display(search_result)
+            return render(request, 'blog/home_search.html', {"MyHomeSearchForm" : MyHomeSearchForm})
+            # return render(request, 'blog/search_result.html', {"search_result" : search_result})
     else:
+        print("get triggered")
         MyHomeSearchForm = HomeSearchForm()
-        # search_term = request.POST['search_term']
-        return render(request, 'blog/home_search.html')    # return render(request, 'blog/search_result.html', {"search_term" : search_term})
+        return render(request, 'blog/home_search.html', {"MyHomeSearchForm" : MyHomeSearchForm})    # return render(request, 'blog/search_result.html', {"search_term" : search_term})
 
-# def search_result(request):
-
-
-    # return render(request, 'blog/search_result.html' , { 'search_term': search_term})    # return render(request, 'blog/search_result.html', {"search_term" : search_term})
+def search_result_display(search_result):
+    
