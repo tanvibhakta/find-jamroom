9c4ea48c37f05c52719cdc5da0098acf12d83430
tanvibhakta <tanvibhakta@gmail.com>
2017-11-24 08:50:46 +0530

    add view to return search_term

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# from .models import Owner, Jamroom, OperatingHours, Customer, Booking
+from blog.forms import HomeSearchForm
+
 def home_search(request):
-    return render(request, 'blog/home_search.html')
+    search_term = "wydhrvdgwir3t532t7y"
+    if request.method == "POST":
+        MyHomeSearchForm = HomeSearchForm(request.POST)
+        if MyHomeSearchForm.is_valid():
+            search_term = MyHomeSearchForm.get("search_term", "")
+    else:
+        MyHomeSearchForm = HomeSearchForm()
+
+    return render(request, 'blog/search_result.html', {"search_term" : search_term})
