99281f7ccb896beb1369f5b2eb7ce2feaa95da24
tanvibhakta <tanvibhakta@gmail.com>
2017-11-24 10:05:55 +0530

    try to debug pages issue

--- blog/views.py
+++ blog/views.py
@@ -7,11 +7,20 @@ from blog.forms import HomeSearchForm
 
 def home_search(request):
     search_term = "wydhrvdgwir3t532t7y"
+    MyHomeSearchForm="db5f61g8yh87d4675fb76g"
     if request.method == "POST":
+        print("triggered")
         MyHomeSearchForm = HomeSearchForm(request.POST)
         if MyHomeSearchForm.is_valid():
-            search_term = MyHomeSearchForm.get("search_term", "")
+            # search_term = MyHomeSearchForm.g
+    # return response(request, 'blog/search_result.html' , { 'search_term': search_term})
+            return render(request, 'blog/search_result.html', {"search_term" : search_term})
     else:
         MyHomeSearchForm = HomeSearchForm()
+        # search_term = request.POST['search_term']
+        return render(request, 'blog/home_search.html')    # return render(request, 'blog/search_result.html', {"search_term" : search_term})
 
-    return render(request, 'blog/search_result.html', {"search_term" : search_term})
+# def search_result(request):
+
+
+    # return render(request, 'blog/search_result.html' , { 'search_term': search_term})    # return render(request, 'blog/search_result.html', {"search_term" : search_term})
