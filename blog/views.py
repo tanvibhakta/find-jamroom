bc010f464f5654859b0c11610ea2819e6b412649
tanvibhakta <tanvibhakta@gmail.com>
2017-12-06 00:14:47 +0530

    finally fix FK associations for single key issues, add bunch more views to handle new buttons

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 from .models import Owner, Jamroom, OperatingHours, Customer, Booking
 
 from blog.forms import HomeSearchForm
 
 def home_search(request):
-    search_result = "search_result wasn't initialised"
-    MyHomeSearchForm="MyHomeSearchForm wasn't initialised"
+    # search_result = "search_result wasn't initialised"
+    # MyHomeSearchForm="MyHomeSearchForm wasn't initialised"
     if request.method == "POST":
         MyHomeSearchForm = HomeSearchForm(request.POST)
         if MyHomeSearchForm.is_valid():
             search_result = request.POST.get("search_term")
-            search_result_display(search_result)
-            return render(request, 'blog/home_search.html', {"MyHomeSearchForm" : MyHomeSearchForm})
-            # return render(request, 'blog/search_result.html', {"search_result" : search_result})
+            # jamrooms = Jamroom.objects.all()
+            # owners = Owner.objects.all()
+            # for  jamroom in jamrooms:
+            jamrooms = Jamroom.objects.filter(name__contains=search_result)
+                # owners[i] = Owner.objects.filter(jamroom__owner_id__contains=jamroom.owner_id)
+            owners = Owner.objects.filter(jamroom__name__contains=search_result)
+            if jamrooms:
+                return render(request, 'blog/search_result_display.html', {"jamrooms" : jamrooms, "owners" : owners})
+        else:
+            return render(request, 'blog/no_search_term.html')
     else:
         print("get triggered")
         MyHomeSearchForm = HomeSearchForm()
         return render(request, 'blog/home_search.html', {"MyHomeSearchForm" : MyHomeSearchForm})    # return render(request, 'blog/search_result.html', {"search_term" : search_term})
 
-def search_result_display(search_result):
-    
+def list_all():
+    return render( 'blog/list_all.html')
+
+def book():
+    return render('blog/book.html')
+
+def booking_confirmed(request):
+    return render(request, 'blog/booking_confirmed.html')
