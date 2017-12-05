5d2f5d5c922aa3cb33345acaaf56a7bfc75b7e03
tanvibhakta <tanvibhakta@gmail.com>
2017-12-05 15:55:40 +0530

    change some orders, add __str__ functions for admin pretty play

--- blog/models.py
+++ blog/models.py
@@ -5,15 +5,22 @@ from django.utils import timezone
 
 class Owner(models.Model):
     """docstring for Owner."""
-    owner_name = models.CharField(max_length=50)
-    owner_ph_no = models.CharField(max_length=10)
+    owner_name = models.CharField(max_length=100)
+    owner_ph_no = models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.owner_name
 
 class Jamroom(models.Model):
-    owner_id = models.ForeignKey(Owner)
-    name = models.CharField(max_length=50)
+    # id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
     location = models.TextField()
     price_per_hour = models.DecimalField(max_digits=6, decimal_places=2)
+    owner_id = models.ForeignKey(Owner)
     # operating_hours = models.ForeignKey(OperatingHours)
+
+    def __str__(self):
+        return self.name
 
 class OperatingHours(models.Model):
     """docstring for Operating_hours."""
@@ -23,16 +30,25 @@ class OperatingHours(models.Model):
     start_hour = models.TimeField(auto_now=False, auto_now_add=False)
     end_hour = models.TimeField(auto_now=False, auto_now_add=False)
 
+    def __str__(self):
+        return self.jamroom_id
+
 class Customer(models.Model):
     """docstring for Customer."""
-    customer_name = models.CharField(max_length=50)
+    customer_name = models.CharField(max_length=100)
     booking_istrue = models.BooleanField()
+
+    def __str__(self):
+        return self.customer_name
 
 class Booking(models.Model):
     """docstring for Booking."""
     customer_id = models.ForeignKey(Customer)
     jamroom_id = models.ForeignKey(Jamroom)
     booked_time_slot = models.TimeField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.customer_id
 
 """
     created_date = models.DateTimeField(
