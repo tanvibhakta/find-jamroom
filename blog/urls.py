985481312f4be34378ae8e7e0e6d101421965015
tanvibhakta <tanvibhakta@gmail.com>
2017-12-06 00:13:43 +0530

    attempt to add new urls to deal with new buttons. no worky

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home_search, name='home_search'),
+    url(r'^list_all$', views.list_all, name='list_all'),
+    url(r'^book$', views.book, name='book'),
+    # url(r'^blog/search_result_display.html$', views.search_result_display, name='search_result_display'),
     # url(r'^result/$', home_search, name='home_search'),
 
 ]
